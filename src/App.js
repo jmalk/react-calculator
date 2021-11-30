054eb1bea8b100f3cf339f53caff1682e195d7a1
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2021-11-30 18:55:17 +0000

    Remove old UI-less function

--- src/App.js
+++ src/App.js
@@ -6,50 +6,6 @@ const Operators = {
   EQUALS: "EQUALS",
 };
 
-const Calculator = () => {
-  let state = {
-    value: 0,
-    previous: 0,
-    operation: null,
-    waiting: false,
-  };
-  return {
-    press: (input) => {
-      if (typeof input === "number") {
-        if (state.waiting) {
-          state.waiting = false;
-          state.previous = state.value;
-          state.value = input;
-        } else {
-          state.value = state.value * 10 + input;
-        }
-      }
-
-      if (input === Operators.PLUS) {
-        state.operation = Operators.PLUS;
-        state.waiting = true;
-      }
-
-      if (input === Operators.MINUS) {
-        state.operation = Operators.MINUS;
-        state.waiting = true;
-      }
-
-      if (input === Operators.EQUALS) {
-        if (state.operation === Operators.PLUS) {
-          state.value = state.previous + state.value;
-        }
-        if (state.operation === Operators.MINUS) {
-          state.value = state.previous - state.value;
-        }
-        state.operation = null;
-      }
-    },
-
-    getDisplay: () => state.value,
-  };
-};
-
 function NumberButton({ handler, n }) {
   return <button onClick={() => handler(n)}>{n}</button>;
 }
