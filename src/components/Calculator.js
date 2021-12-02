f9c74fcc09ecd64294eb7a5df92b05e63eb8bc57
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2021-12-02 18:17:09 +0000

    Componentize operation buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -15,6 +15,14 @@ function NumberButton({ handler, n }) {
   );
 }
 
+function OperationButton({ handler, symbol }) {
+  return (
+    <button className="button" onClick={handler}>
+      {symbol}
+    </button>
+  );
+}
+
 function Calculator() {
   const [value, setValue] = useState(0);
   const [previous, setPrevious] = useState(0);
@@ -69,36 +77,30 @@ function Calculator() {
         <NumberButton handler={handleNumber} n={7} />
         <NumberButton handler={handleNumber} n={8} />
         <NumberButton handler={handleNumber} n={9} />
-        <button
-          className="button"
-          onClick={() => handleOperation(Operators.DIVIDE)}
-        >
-          /
-        </button>
+        <OperationButton
+          handler={() => handleOperation(Operators.DIVIDE)}
+          symbol="/"
+        />
       </div>
 
       <div className="row">
         <NumberButton handler={handleNumber} n={4} />
         <NumberButton handler={handleNumber} n={5} />
         <NumberButton handler={handleNumber} n={6} />
-        <button
-          className="button"
-          onClick={() => handleOperation(Operators.MULTIPLY)}
-        >
-          X
-        </button>
+        <OperationButton
+          handler={() => handleOperation(Operators.MULTIPLY)}
+          symbol="X"
+        />
       </div>
 
       <div className="row">
         <NumberButton handler={handleNumber} n={1} />
         <NumberButton handler={handleNumber} n={2} />
         <NumberButton handler={handleNumber} n={3} />
-        <button
-          className="button"
-          onClick={() => handleOperation(Operators.MINUS)}
-        >
-          -
-        </button>
+        <OperationButton
+          handler={() => handleOperation(Operators.MINUS)}
+          symbol="–"
+        />
       </div>
 
       <div className="row">
@@ -109,15 +111,11 @@ function Calculator() {
         <button className="equals button" onClick={handleEquals}>
           =
         </button>
-        <button
-          className="button"
-          onClick={() => handleOperation(Operators.PLUS)}
-        >
-          +
-        </button>
+        <OperationButton
+          handler={() => handleOperation(Operators.PLUS)}
+          symbol="+"
+        />
       </div>
-
-      <div className="row"></div>
     </div>
   );
 }
