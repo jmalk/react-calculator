c1deb5ea4fc2bd29e6e42400e5fcf1773240c21a
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2021-12-04 21:42:28 +0000

    Add some more tests

--- src/components/Calculator.test.tsx
+++ src/components/Calculator.test.tsx
@@ -31,3 +31,29 @@ test("1+1= displays 2", () => {
 
   expectOutput("2");
 });
+
+test(`1+1 without pressing "=" displays 1`, () => {
+  render(<Calculator />);
+
+  clickButtons(["1", "+", "1"]);
+
+  expectOutput("1");
+});
+
+test(`1+ displays 1`, () => {
+  render(<Calculator />);
+
+  clickButtons(["1", "+"]);
+
+  expectOutput("1");
+});
+
+test("Pressing 9-5= displays 4", () => {
+  render(<Calculator />);
+
+  // Note the minus is a minus symbol not a regular dash.
+  // You may need to copy-paste it specifically!
+  clickButtons(["9", "−", "5", "="]);
+
+  expectOutput("4");
+});
