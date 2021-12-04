70a0e6260b01f11693322965cca56160bc8ebf2b
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2021-12-04 21:25:56 +0000

    Add a UI test with React Testing Library

--- src/components/Calculator.test.tsx
+++ src/components/Calculator.test.tsx
@@ -1,7 +1,17 @@
 import ReactDOM from "react-dom";
+import { render, fireEvent, screen } from "@testing-library/react";
 import Calculator from "./Calculator";
 
 test("Calculator renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Calculator />, div);
+});
+
+test("Pressing 1 twice displays 11", async () => {
+  render(<Calculator />);
+
+  fireEvent.click(screen.getByRole("button", { name: "1" }));
+  fireEvent.click(screen.getByRole("button", { name: "1" }));
+
+  expect(screen.getByRole("status")).toHaveTextContent("11");
 });
